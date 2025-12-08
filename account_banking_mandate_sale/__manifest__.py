b4d219d77a95df02109adf69abcac1e07e86bcfe
Carlos Aguilar <gerencia@aiguesvida.cat>
2025-12-08 23:27:37 +0100

    Bump remaining module versions to 19.0

--- account_banking_mandate_sale/__manifest__.py
+++ account_banking_mandate_sale/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Banking Mandate Sale",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Banking addons",
     "license": "AGPL-3",
     "summary": "Adds mandates on sale orders",
